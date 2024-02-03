17d91e7b2fe0c7c482879d111cc91142fa5d923c
AMI2080 <AMI_2080@yahoo.com>
2024-02-03 17:01:49 +0200

    add show profile page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,11 @@ import { HomeComponent } from './features/home/home.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   {
+    path: 'profile',
+    loadChildren: () =>
+      import('./features/profile/profile.module').then((m) => m.ProfileModule),
+  },
+  {
     path: 'requests',
     loadChildren: () =>
       import('./features/request/request.module').then((m) => m.RequestModule),
