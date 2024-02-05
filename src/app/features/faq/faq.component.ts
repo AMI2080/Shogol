f399c6528cf4e3d9f59fcd42ea520115836fb862
entsarahmed <entesara500@gmail.com>
2024-02-05 08:45:08 +0200

    init page

--- src/app/features/faq/faq.component.ts
+++ src/app/features/faq/faq.component.ts
@@ -1,7 +1,23 @@
-import { Component } from '@angular/core';
+
+import { Component,ViewChild, ElementRef} from '@angular/core';
 
 @Component({
   templateUrl: './faq.component.html',
 })
 export class FAQComponent {
+  @ViewChild('element1') element1!: ElementRef;
+  @ViewChild('element2') element2!: ElementRef;
+  @ViewChild('element3') element3!: ElementRef;
+  @ViewChild('element4') element4!: ElementRef;
+
+  elementsVisible = [true, false, false, false];
+
+  toggleElement(index: number): void {
+    this.elementsVisible[index] = !this.elementsVisible[index];
+  }
+
+  getIconClass(index: number): string {
+    return this.elementsVisible[index] ? 'fas fa-minus' : 'fas fa-plus';
+  }
+  
 }
