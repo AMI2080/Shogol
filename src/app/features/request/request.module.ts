118391e7e4a7e3c1deb29ab1a1955e912a3adf46
Abd-alRahim-Kojan <abdalrahimk083@gmail.com>
2024-02-06 23:55:05 +0300

    replace svg files and replace select function with ng-select

--- src/app/features/request/request.module.ts
+++ src/app/features/request/request.module.ts
@@ -5,9 +5,16 @@ import { RequestComponent } from './request.component';
 import { NewComponent } from './new/new.component';
 import { RequestRoutingModule } from './request-routing.module';
 import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, SharedModule, RequestRoutingModule, NgSelectModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RequestRoutingModule,
+    NgSelectModule,
+    FormsModule,
+  ],
   declarations: [RequestComponent, NewComponent],
 })
 export class RequestModule {}
