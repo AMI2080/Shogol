304cf3139e4566679f23082bc2bf9f8aa1d058d6
Abd-alRahim-Kojan <abdalrahimk083@gmail.com>
2024-02-08 08:38:43 +0300

    fix errors

--- src/app/features/terms-of-use/terms-of-use.component.ts
+++ src/app/features/terms-of-use/terms-of-use.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-terms-of-use',
   templateUrl: './terms-of-use.component.html',
 })
-export class TermsOfUseComponent {
-
-}
+export class TermsOfUseComponent {}
