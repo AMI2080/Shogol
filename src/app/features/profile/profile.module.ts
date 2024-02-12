5b3851b91bb1687e3ed3d2b44b768be07815db73
Abd-alRahim-Kojan <abdalrahimk083@gmail.com>
2024-02-12 20:29:23 +0300

    add daty types + delete scss file

--- src/app/features/profile/profile.module.ts
+++ src/app/features/profile/profile.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../../shared/shared.module';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
@@ -7,7 +8,7 @@ import { OffersComponent } from './offers/offers.component';
 import { MyProfileComponent } from './my-profile/my-profile.component';
 
 @NgModule({
-  imports: [CommonModule, SharedModule, ProfileRoutingModule],
+  imports: [CommonModule, FormsModule, SharedModule, ProfileRoutingModule],
   declarations: [ProfileComponent, MyProfileComponent, OffersComponent],
 })
 export class ProfileModule {}
